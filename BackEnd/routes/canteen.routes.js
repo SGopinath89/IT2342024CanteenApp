583f8ee52be596e045637b6df9de94da996d4a50
anuradhya <anudew16@gmail.com>
2024-06-26 00:32:57 +0530

    Creating canteen.routes-create route

---
+++ BackEnd/routes/canteen.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const {create} = require("../services/canteen.services")
+
+
+router.post("/", create)
+
+
+module.exports=router
