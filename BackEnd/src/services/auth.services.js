6e1c4f3a440b9d1d744cfd723d51c1b0ae11f384
TharinduLakshimal <tharindulakshimal@gmail.com>
2024-06-26 01:51:13 +0530

    feat:creat auth.services register function

---
+++ BackEnd/src/services/auth.services.js
@@ -0,0 +1,51 @@
+const usermodel = require("../models/user.model.js");
+const bcrypt = require("bcryptjs");
+
+
+const register = async (req, res) => {
+  try {
+    const { registrationnumber, username, password, telephone, usertype } =
+      req.body;
+    //validate
+    if (
+      !registrationnumber ||
+      !username ||
+      !password ||
+      !telephone ||
+      !usertype
+    ) {
+      return res
+        .status(500)
+        .send({ success: false, message: "Please Provide All Fields" });
+    }
+    const checkexistsuser = await usermodel.findOne({ registrationnumber });
+    if (checkexistsuser) {
+      return res.status(500).send({
+        success: false,
+        message: "You are already registered please login",
+      });
+    }
+    //hashing password
+    var salt = bcrypt.genSaltSync(10);
+    const hashpassword = await bcrypt.hash(password, salt);
+    //create new user
+    const user = await usermodel.create({
+      registrationnumber,
+      username,
+      password: hashpassword,
+      telephone,
+      usertype,
+    });
+
+    res
+      .status(201)
+      .send({ success: true, message: "Successfully Registered", user });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .send({ sucess: false, message: "error in registered API", err });
+  }
+};
+
+module.exports=register;
