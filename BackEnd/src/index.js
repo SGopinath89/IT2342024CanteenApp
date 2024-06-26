ab607e44c53531213a9f10f269d3f60dbd748b44
TharinduLakshimal <tharindulakshimal@gmail.com>
2024-06-27 01:22:01 +0530

    feat:create user.route.js and getallusers route

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -4,11 +4,13 @@ const { PORT } = require("./utils/config");
 const connectDB = require("./database/config");
 const dotenv = require('dotenv');
 const authroutes = require('./routes/auth.routes')
+const userroutes = require('./routes/user.route')
 
 dotenv.config();
 connectDB();
 
 app.use("/auth",authroutes)
+app.use("/user",userroutes)
 
 app.listen(PORT, () => {
   console.log("API is running on port", PORT);
