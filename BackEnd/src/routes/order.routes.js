67c69d770eba79c4624bccade505a21f9eeefca7
W A D G H Athukorala <gimanthaheshan2000@gmail.com>
2024-06-27 20:32:52 +0530

    add: updateorder functon to routes

--- BackEnd/src/routes/order.routes.js
+++ BackEnd/src/routes/order.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {placeorder} = require("../services/order.services")
+const {placeorder,updateorder} = require("../services/order.services")
 
 
 router.post("/",placeorder)
+router.put("/:id",updateorder)
 
 module.exports=router  
