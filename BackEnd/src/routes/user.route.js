e3542dd414072a89a405161fd6fca08784632118
TharinduLakshimal <tharindulakshimal@gmail.com>
2024-06-28 23:09:42 +0530

    feat:create getuser function

--- BackEnd/src/routes/user.route.js
+++ BackEnd/src/routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {getallusers} = require("../services/user.services")
+const {getallusers,getuser} = require("../services/user.services")
 
 
 
 router.get("/",getallusers)
+router.get("/:id",getuser)
 
 
 module.exports=router
--- BackEnd/src/services/user.services.js
+++ BackEnd/src/services/user.services.js
@@ -21,8 +21,32 @@ const usermodel = require("../models/user.model");
     }
   };
 
-
+  const getuser = async (req, res) => {
+    try {
+      //find user
+      const id = req.params.id;
+      const user = await usermodel.findById(id);
+      if (!user) {
+        return res
+          .status(404)
+          .send({ success: false, message: "User Not Found" });
+      }
+      //hide password
+      user.password = undefined;
+      //response
+      res
+        .status(200)
+        .send({ success: true, message: "User Data Get Successfully", user });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        success: false,
+        message: "Error in get user API",
+      });
+    }
+  };
   module.exports={
-    getallusers
+    getallusers,
+    getuser
 
   }
