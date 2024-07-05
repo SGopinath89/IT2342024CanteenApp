2b6b931887127c4f2a9835c2f42dd6d5b26b7b36
chamithjeewantha <chamithjeewa123@gmail.com>
2024-07-05 22:08:15 +0530

    feat:add verifyToken into routes and check

--- BackEnd/src/routes/canteen.routes.js
+++ BackEnd/src/routes/canteen.routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const {create,getcanteens,getcanteen,deletecanteen,updatecanteen} = require("../services/canteen.services")
+const {verifyToken} = require("../middlewares/auth.middleware")
 
-
-router.post("/", create)
-router.get("/", getcanteens)
-router.get("/:id",getcanteen)
-router.delete("/:id",deletecanteen)
-router.put("/:id",updatecanteen)
+router.post("/",verifyToken, create)
+router.get("/",verifyToken, getcanteens)
+router.get("/:id",verifyToken,getcanteen)
+router.delete("/:id",verifyToken,deletecanteen)
+router.put("/:id",verifyToken,updatecanteen)
 
 module.exports=router
--- BackEnd/src/routes/food.routes.js
+++ BackEnd/src/routes/food.routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const {addfood,displayfoods,displayfood,deletefood,updatefood} = require("../services/food.services")
+const {verifyToken} = require("../middlewares/auth.middleware")
 
-
-router.post("/", addfood) 
-router.get("/", displayfoods)
-router.get("/:id", displayfood)
-router.delete("/:id", deletefood)
-router.put("/:id", updatefood)
+router.post("/",verifyToken, addfood) 
+router.get("/",verifyToken, displayfoods)
+router.get("/:id",verifyToken, displayfood)
+router.delete("/:id",verifyToken, deletefood)
+router.put("/:id",verifyToken, updatefood)
 
 module.exports=router
