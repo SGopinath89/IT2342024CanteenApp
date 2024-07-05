bc9288eaa2e20286f7189ec47ae7d444c7a176f2
TharinduLakshimal <tharindulakshimal@gmail.com>
2024-07-06 00:51:13 +0530

    add checkadminProtect into routers and check

--- BackEnd/src/routes/payment.routes.js
+++ BackEnd/src/routes/payment.routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const {pay,displaypayment,getpayments} = require("../services/payment.services")
-const {verifyToken} = require("../middlewares/auth.middleware")
+const {verifyToken,canteenAdminProtect} = require("../middlewares/auth.middleware")
 
 router.post("/",verifyToken, pay)
 router.get("/:id",verifyToken, displaypayment)
-router.get("/",verifyToken, getpayments)
+router.get("/",verifyToken,canteenAdminProtect,getpayments)
 
 module.exports=router
