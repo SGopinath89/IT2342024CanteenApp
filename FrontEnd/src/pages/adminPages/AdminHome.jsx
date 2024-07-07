e61d8698cd3c33197bbb2513ea29b9542c36c02d
anuradhya <anudew16@gmail.com>
2024-07-07 12:03:08 +0530

    Modifying AdminHome.jsx

--- FrontEnd/src/pages/adminPages/AdminHome.jsx
+++ FrontEnd/src/pages/adminPages/AdminHome.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const AdminHome = () => {
-  return <div>AdminHome</div>;
+  return (
+    <div>
+      AdminHome
+      <Link to="/admin-canteens">Canteen</Link>
+      <Link to="/admin-users">Users</Link>
+    </div>
+  );
 };
 
 export default AdminHome;
