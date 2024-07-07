201150e065a14f6e16e7bad563c61d2dd8b3af79
TharinduLakshimal <tharindulakshimal@gmail.com>
2024-07-07 16:07:25 +0530

    create adminUpdateCanteen

--- FrontEnd/src/pages/adminPages/AdminHome.jsx
+++ FrontEnd/src/pages/adminPages/AdminHome.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Heading from "../../components/Heading";
+import Button from "../../components/Button";
 
 const AdminHome = () => {
   return (
     <div>
-      AdminHome
-      <Link to="/admin-canteens">Canteen</Link>
-      <Link to="/admin-users">Users</Link>
+      <Heading heading="Admin Portal" />
+      <div className="flex justify-center">
+        <Link to="./canteens">
+          <Button text="Canteens" type="default" />
+        </Link>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Link to="./users">
+          <Button text="Users" type="default" />
+        </Link>
+      </div>
     </div>
   );
 };
-
 export default AdminHome;
